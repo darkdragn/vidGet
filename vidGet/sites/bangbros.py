997de9be1822147b7f9edb86da2a731f1bff2845
Christopher Jackson <darkdragn.cj@gmail.com>
2015-04-01 22:02:36 -0500

    Updated bangbros site listing.

--- vidGet/sites/bangbros.py
+++ vidGet/sites/bangbros.py
@@ -1,9 +1,9 @@
 import re
 
 try:
-    from vidGet.vidsite import memorize, initMech, vidSeries
+    from vidGet.vidsite import memorize, initMech, vidSeries, webpage
 except ImportError:
-    from vidsite import memorize, initMech, vidSeries
+    from vidsite import memorize, initMech, vidSeries, webpage
 
 class bangbros(vidSeries):
     siteTemplate = 'http://beta.members.bangbros.com{}'
@@ -12,9 +12,9 @@ class bangbros(vidSeries):
     
     formatPage = lambda self, x: self.siteTemplate.format(x.a['href'])
     pageList = lambda self: self.soup.findAll('span', class_='echThumbLnk-desc')
-    siteList = lambda self: self.soup.findAll('div', 
-                            class_='vdoThumbHolder')[-1].findAll('span', 
-                            class_='echThumbLnk-desc')
+    siteList = lambda self, web: web.soup.findAll('div', 
+                      class_='vdoThumbHolder')[-1].findAll('span', 
+                      class_='echThumbLnk-desc')
         
     def __init__(self, series, extras=None, cookie=None):
         self.name = series
@@ -26,6 +26,7 @@ class bangbros(vidSeries):
         for i in self.extras:
             if 'site' in i:
                 self.pageList, self.type_ = self.siteList, 'Site'
+                #self.pages = self._sitePages
                 self.seriesTemplate = self.siteTemplate
                 self.siteLoad()
             elif 'title' in i:
@@ -38,6 +39,20 @@ class bangbros(vidSeries):
         self.br.click_link(link=self.br.find_link(text_regex=re.compile('Latest.*')))
         self.source = self.br.response().read()
         
+    def sitePageGet(self, cp=1, lp=None):
+        pages = []
+        url = webpage('/'.join([self.url, 'latest', str(cp)]), self.br)
+        if not lp:
+            lp = int(self.soup.findAll('a', class_='echPagi')[-2].text)
+        pages.extend([self.page(self.formatPage(i), self) 
+                      for i in self.siteList(url)])
+        if not cp == lp:
+                pages.extend(self.sitePageGet(cp+1, lp))
+        return pages
+    @property
+    @memorize
+    def _sitePages(self):
+        return self.sitePageGet()
     @property
     @memorize
     def title(self):
