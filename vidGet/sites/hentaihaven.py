df78c21ff00b77628c1db3a87c735358488c954b
Christopher Jackson <darkdragn.cj@gmail.com>
2015-06-12 03:51:45 -0500

    Added support for hentaihaven.

---
+++ vidGet/sites/hentaihaven.py
@@ -0,0 +1,27 @@
+import re
+try:
+    from vidGet.vidsite import vidSeries
+except ImportError:
+    from vidsite import vidSeries
+
+class hentaihaven(vidSeries):
+    siteTemplate   = 'http://hentaihaven.org{}'
+    seriesTemplate = siteTemplate.format('/series/{}/?sort=title')
+    tags  = ['hh', 'hentaihaven']
+    
+    @property
+    def pages(self):
+        return [self.page(i.a['href']) for i in self.soup.findAll('h3')]
+        
+    class page(vidSeries.page):
+        @property
+        def video(self):
+            pref  = ['720p', '360p']
+            links = self.soup.find('div', class_='download_feed_link')
+            return next(o['href'] for i in pref 
+                        for o in links.findAll('a') 
+                        if i in o.span.text)
+        #@property
+        #def url(self):
+            #return self.series.siteTemplate.format(''.join(['/', self.series.name, 
+                                                   #'-episode-', str(self.episode)]))
