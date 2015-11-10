120b301ac07f60d3b64a1a58c5e2e3b7d11e8224
Christopher Jackson <darkdragn.cj@gmail.com>
2015-11-09 21:26:23 -0600

    Updates to NA.

--- vidGet/sites/naughtyamerica.py
+++ vidGet/sites/naughtyamerica.py
@@ -15,21 +15,43 @@ class naughtyamerica(vidSeries):
     def __init__(self, series, extras=None, cookie=None):
         self.name = series
         self.br = initMech(self.siteTemplate.format(''), cookie)
+        if extras:
+            self.extras = extras.split(',')
+            self.runExtras()
     
+    def runExtras(self):
+        for i in self.extras:
+            if 'site' in i:
+                if '=' in i:
+                    self.last = i.split('=')[-1]
+                    self.serType = 'sites'
+                self.type_ = 'Site'
+                self.seriesTemplate = self.siteTemplate.format('/sites/{}/?')
+            elif 'title' in i:
+                self.title = i.split('=')[-1]
+            elif 'pref' in i:
+                self.pref = i.split('=')[-1]
+
     @property
     def title(self):
-        return self.soup.find('p', 
-                              class_='pornstar-name').text.replace(u'\xa0', '_')
+        return self.soup.find(
+            'p', 
+            class_='pornstar-name'
+            ).text.replace(u'\xa0', '_')
     @property
     @memorize
     def pages(self):
         pages =  [self.page(i.a['href'], self) 
                  for i in self.soup.findAll('p', class_='pornstar-name') ]
-        try:
-            otherPages = [i['href'] for i in self.soup.find('div', id="pagination_more_porn_videos").findAll('a') 
-                          if not i.has_attr("class")]
-        except AttributeError:
-            return pages
+        if hasattr(self, 'last'):
+            base = '/'.join(['/{}'.format(self.serType), self.name, '?&p={}'])
+            otherPages = [base.format(i) for i in xrange(1, 5)]
+        else:
+            try:
+                otherPages = [i['href'] for i in self.soup.find('div', id="pagination_more_porn_videos").findAll('a') 
+                              if not i.has_attr("class")]
+            except AttributeError:
+                return pages
         for i in otherPages:
             hold = webpage(self.siteTemplate.format(i), self.br)
             pages = pages + [self.page(i.a['href'], self) 
@@ -40,14 +62,6 @@ class naughtyamerica(vidSeries):
     #def preview(self):
     #    return [self.soup.find('div', class_='picture-container').img['src'],
     #            self.soup.find('div', class_='right-banner-background')['style'].split('\'')[1]]
-                
-    #def runExtras(self):
-    #    for i in self.extras:
-    #        if 'user' in i:
-    #            self.username = i.split('=')[-1]
-    #        if 'pass' in i:
-    #            self.password = i.split('=')[-1]
-        
     class page(vidSeries.page):
         @property
         @memorize
